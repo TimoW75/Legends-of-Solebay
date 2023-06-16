99c3e557d9824740d03b1362bc218c2f35c08f26
sharon <svanopzeeland@kpnmail.nl>
2023-06-16 11:44:40 +0200

    everything done except for PreviousButton and font

--- Assets/Scripts/WaterBoat.cs
+++ Assets/Scripts/WaterBoat.cs
@@ -1,5 +1,4 @@
-﻿using Ditzelgames;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,7 +20,6 @@ public class WaterBoat : MonoBehaviour
     //internal Properties
     protected Vector3 CamVel;
 
-    [SerializeField] private FixedJoystick _joystick;
 
     public void Awake()
     {
@@ -30,35 +28,7 @@ public class WaterBoat : MonoBehaviour
         StartRotation = Motor.localRotation;
     }
 
-    public void FixedUpdate()
-    {
-        //default direction
-        var forceDirection = transform.forward;
-
-        var steer = Mathf.RoundToInt(-_joystick.Horizontal);
-
-        // forward/backward power (invert the vertical input)
-        var throttle = Mathf.Clamp01(-_joystick.Vertical * -1);
-
-        Rigidbody.AddForceAtPosition(steer * transform.right * SteerPower / 100f, Motor.position);
-
-        PhysicsHelper.ApplyForceToReachVelocity(Rigidbody, transform.forward * MaxSpeed * throttle, Power);
-
-        //Motor Animation // Particle system
-        Motor.SetPositionAndRotation(Motor.position, transform.rotation * StartRotation * Quaternion.Euler(0, 30f * steer, 0));
-        if (ParticleSystem != null)
-        {
-            if (Mathf.RoundToInt(-_joystick.Horizontal) != 0)
-                ParticleSystem.Play();
-            else
-                ParticleSystem.Pause();
-        }
-
-        //moving forward
-        var movingForward = Vector3.Cross(transform.forward, Rigidbody.velocity).y < 0;
-
-        //move in direction
-        Rigidbody.velocity = Quaternion.AngleAxis(Vector3.SignedAngle(Rigidbody.velocity, (movingForward ? 1f : 0f) * transform.forward, Vector3.up) * Drag, Vector3.up) * Rigidbody.velocity;
-    }
+    
+        
 
 }
