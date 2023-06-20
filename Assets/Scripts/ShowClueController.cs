3f36b59fb66b18201c6430076998ecb633b1728a
Timo werkhoven <timo2003werkhoven@gmail.com>
2023-06-20 12:37:39 +0200

    Finding clue's added

--- Assets/Scripts/ShowClueController.cs
+++ Assets/Scripts/ShowClueController.cs
@@ -1,23 +1,51 @@
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShowClueController : MonoBehaviour
 {
     [SerializeField] private Image[] images;
-    void Start()
+    [SerializeField] private float fadeDuration = 1f;
+
+    private void Start()
+    {
+        DisableAllImages();
+    }
+
+    public void EnableImage(int index)
+    {
+        StartCoroutine(FadeImage(images[index], 0f, 1f, fadeDuration));
+    }
+
+    private void DisableAllImages()
     {
         foreach (Image image in images)
         {
-            image.enabled = false;
+            image.gameObject.SetActive(false);
         }
-        ShowClue(0);
-
     }
 
-
-    public void ShowClue(int index)
+    private IEnumerator FadeImage(Image image, float startAlpha, float endAlpha, float duration)
     {
-        images[index].enabled = true;
+        image.gameObject.SetActive(true);
+
+        Color color = image.color;
+        color.a = startAlpha;
+        image.color = color;
+
+        float startTime = Time.time;
+        float endTime = startTime + duration;
+
+        while (Time.time <= endTime)
+        {
+            float t = (Time.time - startTime) / duration;
+            color.a = Mathf.Lerp(startAlpha, endAlpha, t);
+            image.color = color;
+            yield return null;
+        }
+
+        color.a = endAlpha;
+        image.color = color;
     }
 }
