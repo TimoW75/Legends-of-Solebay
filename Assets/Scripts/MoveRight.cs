5f4dc7ab4486b584f00d831071011912a3094424
Timo werkhoven <timo2003werkhoven@gmail.com>
2023-06-29 12:05:10 +0200

    Onboarding added

--- Assets/Scripts/MoveRight.cs
+++ Assets/Scripts/MoveRight.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class MoveRight : MonoBehaviour
 {
-    public float speed = 1f;  // The speed at which the image moves
-
-    private RectTransform rectTransform;
-    private float targetX;
+    public float distance = 5f; // Distance to move
+    public float speed = 1f; // Speed of movement
+    private Vector3 startPos; // Starting position
+    private float targetPos; // Target position
+    private bool movingRight = true; // Flag to determine movement direction
 
     private void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
-        targetX = rectTransform.anchoredPosition.x + 200f;  // Adjust the distance as needed
+        startPos = transform.position; 
+        targetPos = startPos.x + distance; 
     }
 
     private void Update()
     {
-        float currentX = rectTransform.anchoredPosition.x;
-        float newX = Mathf.MoveTowards(currentX, targetX, speed * Time.deltaTime);
-        rectTransform.anchoredPosition = new Vector2(newX, rectTransform.anchoredPosition.y);
+        float step = speed * Time.deltaTime;
+
+        if (movingRight)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetPos, startPos.y, startPos.z), step);
+            if (transform.position.x >= targetPos)
+            {
+                movingRight = false; // Change direction
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos, step);
+            if (transform.position.x <= startPos.x)
+            {
+                movingRight = true; // Change direction
+            }
+        }
     }
+
 }
